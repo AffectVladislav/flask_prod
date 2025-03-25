aa3614c9ab5556aed5c2223fab8e75028d5409cb
vladh <vlad.hagai@mail.ru>
2025-03-26 00:11:11 +0300

    new models 'one to many'

--- app/models/user.py
+++ app/models/user.py
@@ -2,4 +2,10 @@ from ..extensions import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    posts = db.relationship(Post, backref='author', lazy='dynamic')
+    status = db.Column(db.Boolean, nullable=False, default=False)
     username = db.Column(db.String(50), unique=True, nullable=False)
+    login = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    avatar = db.Column(db.String(150), nullable=False)
