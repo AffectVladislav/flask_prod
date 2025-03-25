76c1676d592f55dbb4e0fb44381145d96d25506e
vladh <vlad.hagai@mail.ru>
2025-03-26 00:16:58 +0300

    created def login

--- app/routes/user.py
+++ app/routes/user.py
@@ -10,6 +10,21 @@ def create_user(name):
     db.session.add(new_user)
     db.session.commit()
     return 'User Created Successfully'
+@user.route('/user/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(login=form.login.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            flash(f"{form.login.data}: Успешный вход!")
+            return redirect(next_page) if next_page else redirect(url_for('post.all'))
+        else:
+            flash(f"При входе произошла ошибка", "danger")
+    return render_template('user/login.html', form=form)
+
+@user.route('/user/logout', methods=['GET', 'POST'])
 def logout():
     logout_user()
     return redirect(url_for('post.all'))
