aad299875e5d0ba729a3f20c354956c80a29ce02
vladh <vlad.hagai@mail.ru>
2025-03-26 00:12:52 +0300

    created decorator and def load_user

--- app/models/user.py
+++ app/models/user.py
@@ -2,6 +2,11 @@ from ..extensions import db
 from flask_login import UserMixin
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     posts = db.relationship(Post, backref='author', lazy='dynamic')
