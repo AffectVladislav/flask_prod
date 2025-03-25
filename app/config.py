e1f356523c1fd965d7855d43472a9006d4840160
vladh <vlad.hagai@mail.ru>
2025-03-26 00:21:16 +0300

    minor edits and there will be a docker

--- app/config.py
+++ app/config.py
@@ -1,6 +1,12 @@
 import os
 
+
 class Config(object):
+    APPNAME = 'app'
+    ROOT = os.path.abspath(APPNAME)
+    UPLOAD_PATH = 'static/uploads/'
+    SERVER_PATH = ROOT + UPLOAD_PATH
+
     USER = os.environ.get('POSTGRES_USER', 'codervlad')
     PASSWORD = os.environ.get('POSTGRES_PASSWORD', 'codervlad')
     HOST = os.environ.get('POSTGRES_HOST', 'localhost')
@@ -8,5 +14,5 @@ class Config(object):
     DATABASE = os.environ.get('POSTGRES_DB', 'mydb')
 
     SQLALCHEMY_DATABASE_URI = f'postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}'
-    SECRET_KEY='sdhsdhsdfhLDGHl543567jkhjkh3kj64h5'
-    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    SECRET_KEY = 'sdhsdhsdfhLDGHl543567jkhjkh3kj64h5'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
