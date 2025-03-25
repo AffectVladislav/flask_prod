034f171ced43aedcc68fe73a3e642c8941f61d12
vladh <vlad.hagai@mail.ru>
2025-03-25 23:40:10 +0300

    created functions.py, created def save_picture include try|except

---
+++ app/functions.py
@@ -0,0 +1,27 @@
+import os
+import secrets
+from PIL import Image, UnidentifiedImageError
+
+from flask import current_app
+
+
+def save_picture(picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(current_app.config['SERVER_PATH'], picture_fn)
+    output_size = (125, 125)
+
+    os.makedirs(os.path.dirname(picture_path), exist_ok=True)
+
+    try:
+        i = Image.open(picture)
+        i.thumbnail(output_size)
+        i.save(picture_path)
+    except UnidentifiedImageError:
+        raise ValueError("Загруженный файл не является изображением")
+
+    except Exception as e:
+        raise ValueError(f"Ошибка при сохранении изображения: {e}")
+
+    return picture_fn
