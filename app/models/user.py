29361e9fe7386d72d97336ad9f47a7b1cb37712c
vladh <vlad.hagai@mail.ru>
2025-03-26 00:11:36 +0300

    add user_mixin

--- app/models/user.py
+++ app/models/user.py
@@ -1,6 +1,8 @@
 from ..extensions import db
+from flask_login import UserMixin
 
-class User(db.Model):
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     posts = db.relationship(Post, backref='author', lazy='dynamic')
     status = db.Column(db.Boolean, nullable=False, default=False)
